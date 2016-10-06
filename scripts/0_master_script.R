87c73e0aee7681acd5efa4f526e1b50a17aa7c29
JonMinton <nate.minton@gmail.com>
2016-10-06 11:20:26 +0100

    add skeleton structure and master script

---
+++ scripts/0_master_script.R
@@ -0,0 +1,30 @@
+# Script 0_0 : Master script 
+# 6 / 10 / 2016
+
+# Description
+
+
+# Standard sequence:
+
+# 1) Clear workspace
+# 2) Load prereq packages 
+# 3) Load raw data (once) 
+# 4) Tidy raw data (once)
+# 5) Load tidied data 
+# 6) Ad hoc analyses 
+# 7) Automated analyses 
+
+# 1) Clear workspace
+rm(list = ls())
+
+# 2) load pre-requisites 
+
+# Using pacman : a package for managing packages 
+
+install.packages("pacman") # Do this first time only
+
+pacman::p_load(
+  
+  
+)
+
